1fb93610a8345603ea8451bcb005d56dfb06829a
SAINATH <SAINATH@DESKTOP-ICPFEOD>
2023-08-28 16:30:50 +0530

    ok

---
+++ Basicjava/src/com/loops/BreakDemo.java
@@ -0,0 +1,17 @@
+package com.loops;
+
+public class BreakDemo {
+
+	public static void main(String[] args)
+	{
+		for(int i=0;i<10;i++)
+		{
+			if(i==5)
+				break;    //comes out of loop
+			System.out.println(i);
+		}
+		
+
+	}
+
+}
